87fd2685a7ac23039c0490c393797bcdbea78752
NhatMinh2208 <minh33haha@gmail.com>
2024-01-09 18:35:17 +0700

    untrackfile-2

--- image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-im = cv2.imread('./psf_mag.tif', cv2.IMREAD_ANYDEPTH | cv2.IMREAD_GRAYSCALE)
-tensor = torch.from_numpy(im)
-print(tensor.shape)
-plt.imshow(im)
-plt.show()
